9c70b83e708985d0f401fc2d9e8bbdbb8b58473a
ottolol <alexsveshnik@gmail.com>
2024-12-30 15:57:01 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from "react";
-import "./App.css";
+import './App.css';
+import styled from 'styled-components';
+
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Portfolio</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Title>Welcome to IT-INCUBATOR</Title>
+        </div>
+    );
 }
 
 export default App;
+
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: #e91e63;
+`;
