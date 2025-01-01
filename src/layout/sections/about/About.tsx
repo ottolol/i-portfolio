c3ea81a2e5284bfecf772f5950b2c203ee9e6d50
ottolol <alexsveshnik@gmail.com>
2025-01-02 01:55:25 +0300

    add About.tsx

---
+++ src/layout/sections/about/About.tsx
@@ -0,0 +1,18 @@
+export function About() {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>
+        The Generator App is an online tool that helps you to export ready-made
+        templates ready to work as your future website. It helps you to combine
+        slides, panels and other components and export it as a set of static
+        files: HTML/CSS/JS.
+      </p>
+      <h2>Work Experience</h2>
+      <p>Junior Web Developer - Full Time</p>
+      <span>icon - Dr. Rajkumar’s Learning App</span>
+      <span>icon - Bengaluru</span>
+      <span>icon - Sep 2021 - Dec 2021</span>
+    </div>
+  );
+}
