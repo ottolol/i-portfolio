eeaedb8d1db53a05c2898b54873b9ca7ad1c7401
ottolol <alexsveshnik@gmail.com>
2025-01-02 01:53:54 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import { Header } from "./layout/header/Header";
+import { About } from "./layout/sections/about/About";
+import { Projects } from "./layout/sections/projects/Projects";
+import { Skills } from "./layout/sections/skills/Skills";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <About />
+      <Skills />
+      <Projects />
     </div>
   );
 }
